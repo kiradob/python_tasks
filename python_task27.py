1da63e8b43609fc3f4a0979edca661e6acdc34d7
kira.dob <arik.dobrynina@yandex.ru>
2023-02-24 15:14:32 +0700

    added task27

--- python_task27.py
+++ python_task27.py
@@ -4,6 +4,13 @@
 # Input: She sells sea shells on the sea shore The shells that she sells are sea shells I'm sure So if she sells sea shells on the sea shore I'm sure that the shells are sea shore shells
 
 # Output: 13
-
+# Функция split создает список, в котором каждое слово записано отдельным элементом
+# set - преобразование списка в множество, в котором не может быть повторяющихся элементов
 print(len(set(input().lower().split())))
 
+# # Вариант 2
+# text = input().split()
+# set_result = set()
+# for i in text:
+#     set_result.add(i.lower())
+# print(len(set_result))
