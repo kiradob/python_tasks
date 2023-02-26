5cf693506bea312c030abee3e11f776e07431f04
kira.dob <arik.dobrynina@yandex.ru>
2023-02-26 23:53:17 +0700

    added task32 homework

--- python_task32_homework.py
+++ python_task32_homework.py
@@ -15,3 +15,11 @@ preset_max = int(input('Задайте значение максимума '))
 print('Проверяемый массив', lst)
 
 print('Индексы элементов массива, значения которых в заданном диапазоне', between_min_max(preset_min, preset_max, lst))
+
+# # Решение преподавателя
+# list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+# min_number = int(input())
+# max_number = int(input())
+# for i in range(len(list_1)):
+#     if min_number <= list_1[i] <= max_number:
+#         print(i)
