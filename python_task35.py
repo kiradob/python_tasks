28777c248cac41e32fa432ece0a03982d5dbea8a
kira.dob <arik.dobrynina@yandex.ru>
2023-02-26 21:42:47 +0700

    added python_task35

---
+++ python_task35.py
@@ -0,0 +1,16 @@
+# Задача №35. Напишите функцию, которая принимает одно число и проверяет, является ли оно простым
+# Напоминание: Простое число - это число, которое имеет 2 делителя: 1 и n(само число)
+# Input: 5
+# Output: yes
+
+def prime(n):
+    i = 2
+    flag=True
+    while i < n and flag:
+        if n % i == 0:
+            flag=False
+        i += 1
+    if flag:
+        return "yes"
+    return "no"
+print(prime(int(input("Введите число: "))))
